1daea60b0e42abd28253a8022a98b0eaedd6692f
Verner Ravil <werner.ravil@gmail.com>
2023-03-07 17:58:38 +0500

    Added one-way navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
     const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
 
     ReactDOM.render(<App history={history} />, el);
 };
@@ -13,7 +17,7 @@ if (process.env.NODE_ENV === "development") {
     const devRoot = document.querySelector("#_marketing-dev-root");
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
