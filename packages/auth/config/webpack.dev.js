a5339d7e944881d7bb58cd4b8f0e46bc9ef6a11a
Verner Ravil <werner.ravil@gmail.com>
2023-03-09 22:05:41 +0500

    added auth to app

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -4,22 +4,22 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',    
+    mode: 'development',
     output: {
-        publicPath: 'http://localhost:8081/'
+        publicPath: 'http://localhost:8082/auth/'
     },
     devServer: {
-        port: 8081,
+        port: 8082,
         historyApiFallback: {
-            index: 'index.html'
+            index: 'http://localhost:8082/index.html'
         }
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'marketing',
+            name: 'auth',
             filename: 'remoteEntry.js',
             exposes: {
-                './MarketingApp': './src/bootstrap'
+                './AuthApp': './src/bootstrap'
             },
             shared: packageJson.dependencies,
         })
--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,9 +4,11 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-    const history = defaultHistory || createMemoryHistory({
-        initialEntries: [initialPath]
-    });
+    const history =
+        defaultHistory ||
+        createMemoryHistory({
+            initialEntries: [initialPath],
+        });
 
     if (onNavigate) {
         history.listen(onNavigate);
@@ -15,18 +17,18 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     ReactDOM.render(<App history={history} />, el);
 
     return {
-        onParentNavigate({pathname: nextPathname}) {
+        onParentNavigate({ pathname: nextPathname }) {
             const { pathname } = history.location;
 
             if (pathname !== nextPathname) {
                 history.push(nextPathname);
             }
-        }
+        },
     };
 };
 
 if (process.env.NODE_ENV === "development") {
-    const devRoot = document.querySelector("#_marketing-dev-root");
+    const devRoot = document.querySelector("#_auth-dev-root");
 
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
